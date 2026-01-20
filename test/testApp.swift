581cb649b00eba535226114d8002a9c296edf409
Kei Tsukamoto <tsukamoto.kei@gmail.com>
2026-01-21 00:13:16 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Kei Tsukamoto  on 2026/01/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
